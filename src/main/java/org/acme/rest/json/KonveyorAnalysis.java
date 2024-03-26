8f5858298b41227055aceeeaa2779cbd39186a53
Luca Molteni <volothamp@gmail.com>
2024-03-26 15:32:03 +0100

    Can pass categories as parameter

--- src/main/java/org/acme/rest/json/KonveyorAnalysis.java
+++ src/main/java/org/acme/rest/json/KonveyorAnalysis.java
@@ -8,18 +8,20 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import net.thisptr.jackson.jq.JsonQuery;
 import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Versions;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jboss.logging.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/konveyor")
 public class KonveyorAnalysis {
@@ -36,9 +38,20 @@ public class KonveyorAnalysis {
     @POST
     @Path("/score")
     @Consumes(MediaType.APPLICATION_JSON)
-    public int score(ObjectNode konveyorAnalysis) throws JsonQueryException {
+    public int countByCategory(ObjectNode konveyorAnalysis, @QueryParam("category") List<String> categories)
+            throws JsonQueryException {
 
-        final JsonQuery query = JsonQuery.compile(".issues[] | select(.category == \"mandatory\")", Versions.JQ_1_6);
+        if (categories == null || categories.isEmpty()) {
+            categories = List.of("mandatory");
+        }
+
+        String categoriesQueryString = categories.stream()
+                                                 .map(c -> String.format(".category == \"%s\"", c))
+                                                 .collect(Collectors.joining(" and "));
+
+        String jqQuery = String.format(".issues[] | select(%s)", categoriesQueryString);
+
+        final JsonQuery query = JsonQuery.compile(jqQuery, Versions.JQ_1_6);
         LOG.info(query.toString());
 
         List<JsonNode> out = new ArrayList<>();
@@ -50,10 +63,11 @@ public class KonveyorAnalysis {
     @GET
     @Path("/{id}/analysis")
     @Consumes(MediaType.APPLICATION_JSON)
-    public int score(@PathParam("id") int id) throws JsonQueryException {
+    public int countByCategory(@PathParam("id") int id, @QueryParam("category") List<String> categories)
+            throws JsonQueryException {
         ObjectNode analysis = konveyorService.analysis(id);
 
-        int score = score(analysis);
+        int score = countByCategory(analysis, categories);
 
         return score;
     }
